8f1db3c4521087a10439bf5d2a383d075f62fa49
SHOAIB CHEEMA <167829955+Shoaibcheema2917@users.noreply.github.com>
2025-03-26 11:04:35 +0500

    Create queries to insert the data in the database

---
+++ insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO Books VALUES (1, 'Inroduction to database', 'Huabo Lu', 'Non Fiction', 'Available');
+INSERT INTO Books VALUES (2, 'Advanced database', 'Shoaib Cheema', 'Non Fiction', 'Available');
+
+INSERT INTO Borrowers VALUES (1, 'john smith', '555-1234', 'John@gmail.com');
+INSERT INTO Borrowers VALUES (2, 'Mary harpar', '555-5678', 'mary@outlook.com');
+
+INSERT INTO Borrow_Records VALUES (1, 1, 1, '2025-03-15', NULL);
+
+INSERT INTO Staff VALUES (1, 'Jane Doe', 'Librarian', 'jane@library.org');
